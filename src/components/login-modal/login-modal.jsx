9ad9fd94702513f40929cfe8df7c2066c624d573
kissmett <kissmett@163.com>
2020-09-04 17:57:43 +0800

    change login-modal from function to React.Component add post account and password

--- src/components/login-modal/login-modal.jsx
+++ src/components/login-modal/login-modal.jsx
@@ -6,30 +6,70 @@ import Box from '../box/box.jsx';
 import SubmitLoginButton from './submit-login-button.jsx';
 import connect from 'react-redux/es/connect/connect';
 import {closeLoginModal} from "../../reducers/modals";
-const LoginModal = props => (
-    <Modal
-        className={styles.modalContent}
-        contentLabel={props.title}
-        id="loginModal"
-        onRequestClose={props.onCancel}
-    >
-        <Box>
-            <input
-                className={styles.minInput}
-                name="account"
-                placeholder="账号"
-                type="text"
-            /><br />
-            <input
-                className={styles.minInput}
-                name="password"
-                placeholder="密码"
-                type="password"
-            /><br />
-            <SubmitLoginButton className={styles.btnSubmit} />
-        </Box>
-    </Modal>
-);
+import bindAll from "lodash.bindall";
+
+
+class LoginModal extends React.Component{
+    constructor (props) {
+        super(props);
+        this.submitform = this.submitform.bind(this);
+
+    }
+    submitform(){
+        let account = document.getElementById('account').value;
+        let password = document.getElementById('password').value;
+        fetch("/", {
+            mode: 'no-cors',
+            method: "POST",
+            headers: {
+                "Content-Type": "multipart/form-data",
+                "Accept": "application/json",
+                "type": "formData",
+            },
+            body: {account:account,password:password}
+        }).then(function (res) {
+            if (res.ok) {
+                alert("Perfect! ");
+            } else if (res.status == 401) {
+                alert("Oops! ");
+            }
+        }, function (e) {
+            alert("Error submitting form!");
+        });
+        console.log("submit form.",account,password);
+    }
+    render() {
+        const props = this.props;
+        return (
+            <Modal
+                className={styles.modalContent}
+                contentLabel={props.title}
+                id="loginModal"
+                onRequestClose={props.onCancel}
+            >
+                <Box>
+                    <input
+                        className={styles.minInput}
+                        name="account"
+                        id="account"
+                        placeholder="账号"
+                        type="text"
+                    /><br/>
+                    <input
+                        className={styles.minInput}
+                        name="password"
+                        id="password"
+                        placeholder="密码"
+                        type="password"
+                    /><br/>
+                    <SubmitLoginButton className={styles.btnSubmit} onClick={this.submitform}/>
+                </Box>
+            </Modal>
+        );
+    }
+
+}
+
 LoginModal.propTypes = {
     title: PropTypes.string.isRequired,
     onCancel:PropTypes.func.isRequired
