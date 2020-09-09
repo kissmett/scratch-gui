cc7aa9a09d701b1db1e955947a69d5ece347500a
kissmett <kissmett@163.com>
2020-09-09 22:26:06 +0800

    add save stage img src to cloud

--- src/containers/sb3-cloudsaver.jsx
+++ src/containers/sb3-cloudsaver.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import {connect} from 'react-redux';
 import {projectTitleInitialState} from '../reducers/project-title';
-import downloadBlob from '../lib/download-blob';
+import VM from 'scratch-vm';
+// import downloadBlob from '../lib/download-blob';
 /**
  * Project saver component passes a downloadProject function to its child.
  * It expects this child to be a function with the signature
@@ -30,11 +31,18 @@ class SB3CloudSaver extends React.Component {
             if (this.props.onSaveFinished) {
                 this.props.onSaveFinished();
             }
+            //截图
+            var renderer = this.props.vm.renderer;
+            var canvas = renderer.canvas;
+            renderer.draw();
+            const img = new Image();
+            img.src = canvas.toDataURL('image/png', 0.7);
             // downloadBlob(this.props.projectFilename, content);
             //upload file to cloud
             console.log('--------saveProjectToCloud.content',content);
             const data = new FormData();
-            data.append('file', content);  //相当于 input:file 中的name属性
+            data.append('file', content);  //file 相当于 input:file 中的name属性
+            data.append('img',img.src);//str
             fetch('/file/upload', {
                 method: 'POST',
                 body: data
@@ -65,7 +73,8 @@ SB3CloudSaver.propTypes = {
     className: PropTypes.string,
     onSaveFinished: PropTypes.func,
     projectFilename: PropTypes.string,
-    saveProjectSb3: PropTypes.func
+    saveProjectSb3: PropTypes.func,
+    vm:PropTypes.instanceOf(VM).isRequired,
 };
 SB3CloudSaver.defaultProps = {
     className: ''
@@ -73,7 +82,8 @@ SB3CloudSaver.defaultProps = {
 
 const mapStateToProps = state => ({
     saveProjectSb3: state.scratchGui.vm.saveProjectSb3.bind(state.scratchGui.vm),
-    projectFilename: getProjectFilename(state.scratchGui.projectTitle, projectTitleInitialState)
+    projectFilename: getProjectFilename(state.scratchGui.projectTitle, projectTitleInitialState),
+    vm:state.scratchGui.vm
 });
 
 export default connect(
